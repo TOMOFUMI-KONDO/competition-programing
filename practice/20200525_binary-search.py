145d30fdc50416d57c544b7ba43f8ada5520a06a
tomokon <ugax2kontomo0314@gmail.com>
2020-05-25 21:21:45 +0900

    learn everyday

---
+++ practice/20200525_binary-search.py
@@ -0,0 +1,32 @@
+import sys
+import copy
+
+def main():
+  n = int(sys.stdin.readline())
+  S = list(map(int, sys.stdin.readline().split()))
+  q = int(sys.stdin.readline())
+  T = list(map(int, sys.stdin.readline().split()))
+
+  C = 0
+  
+  for i in range(q):
+    tmp = T[i]
+    L = 0
+    R = n - 1
+    M = n // 2
+
+    while M >= L:
+      if tmp == S[M]:
+        C += 1
+        break
+      elif tmp > S[M]:
+        L = M + 1
+      else:
+        R = M - 1
+
+      M = (R + L) // 2
+
+  print(C)
+
+if __name__ == '__main__':
+  main()
